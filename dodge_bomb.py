dd8ccef2f99f5f92cf4159cf6ef2593e99c447be
Ishihara <c0b22011b7@edu.teu.ac.jp>
2023-11-28 14:10:01 +0900

    練習1：爆弾Surfaceの追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -11,6 +12,12 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    enn = pg.Surface((20,20))  # 練習1：透明のSurfaceを作る
+    enn.set_colorkey((0, 0, 0))  # 黒い部分の透明化
+    pg.draw.circle(enn, (255,0,0), (10,10), 10)  # 半径10の赤い円
+    bb_rct = enn.get_rect()  # 練習2：rectの抽出
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT) 
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +27,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(enn,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
